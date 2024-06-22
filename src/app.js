3108d00cf6135e3ef25d1fd3b58f51a5a06b61db
kbir079 <ks400068@gmail.com>
2024-06-22 17:43:43 +0530

    Utility Files Done

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+app.use(cors({
+    origin: process.env.CROSS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"})); //For JSON Data
+app.use(express.urlencoded({extended: true,limit : "16kb"})); //For URL decoding [%20 or ?= Sometimes it get error i.e why we used this]
+app.use(express.static("public")) //For serving public files
+app.use(cookieParser())
+
+export {app};
