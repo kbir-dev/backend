992b60222446fd29d8b8d1b0b3d530d581d8a2fc
kbir079 <ks400068@gmail.com>
2024-06-23 16:18:51 +0530

    Setup user and video models

---
+++ src/models/video.models.js
@@ -0,0 +1,42 @@
+import { type } from "express/lib/response";
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+    videoFile: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: {
+        type:String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    duration:{
+        type: Number,
+        required : true
+    },
+    views:{
+        type: Number,
+        default: 0
+    },
+    isPublished:{
+        type: Boolean,
+        default: false
+    }
+},
+    {
+        timestamps: true
+    })
+
+export const Video = mongoose.model("Video", videoSchema)
