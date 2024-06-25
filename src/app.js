096c8cdab8ee756e7a079942a70ef9d50caac173
kbir079 <ks400068@gmail.com>
2024-06-25 18:20:59 +0530

    Setup Complete

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.urlencoded({extended: true,limit : "16kb"})); //For URL decoding
 app.use(express.static("public")) //For serving public files
 app.use(cookieParser())
 
+//Importing Routes
+import userRouter from "./routes/users.routes.js"
+
+//Declaring Routes 
+app.use("/api/v1/users",userRouter)
+
 export {app};
